cc7b52e61d5b6a8b53e076b895720b39fe4f0f4d
Chao Hui <chui@thoughtworks.com>
2017-08-01 19:47:55 +0800

    [huichao] modify the format.sh

--- missed-order/format.sh
+++ missed-order/format.sh
@@ -5,11 +5,11 @@ i=1
 while read line;do
 
 if echo $line | grep topic &> /dev/null;then
-	echo $line | sed "s/^[0-9]/${i}/g"
+	echo $line | sed "s/^[0-9]/${i}/g" >> ./after.txt
 	let i=i+1
 else
-	echo -e "  $line"
+	echo -e "  $line" >> ./after.txt
 fi
 
-done < test.txt
+done < ./before.txt
 
